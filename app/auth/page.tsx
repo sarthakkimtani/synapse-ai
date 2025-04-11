2d1786f4344f0f3fea6694aa2829a30bc348b2bd
Sarthak Kimtani <sarthakkimtani123@gmail.com>
2025-04-11 20:29:44 +0530

    feat: add privacy policy

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 
@@ -21,16 +22,19 @@ export default async function Auth() {
   return (
     <section className="fixed inset-0 overflow-hidden">
       <Image src={Grid} className="-z-10 object-cover" fill alt="Grid" />
-      <div className="flex items-center justify-center w-full h-full">
-        <div className="flex flex-col items-center justify-center w-4/5 md:w-5/12 lg:w-1/3 bg-black border border-primary py-10 px-6 rounded-xl shadow-[0_0_10px_theme(colors.primary)]">
-          <h5 className="text-2xl font-bold text-white text-center">
+      <div className="flex h-full w-full items-center justify-center">
+        <div className="flex w-4/5 flex-col items-center justify-center rounded-xl border border-primary bg-black px-6 py-10 shadow-[0_0_10px_theme(colors.primary)] md:w-5/12 lg:w-1/3">
+          <h5 className="text-center text-2xl font-bold text-white">
             Login or Signup to use Synapse
           </h5>
-          <p className="text-[#7D7D7D] text-md mt-4 text-center">
+          <p className="text-md mt-4 text-center text-[#7D7D7D]">
             Choose your preferred sign in method
           </p>
-          <AuthButton variant="google" className="mt-8 mb-6" />
+          <AuthButton variant="google" className="mb-6 mt-8" />
           <AuthButton variant="github" />
+          <Link href="/privacy-policy" className="mt-6 text-center text-xs text-[#7D7D7D]">
+            By continuing, you have read and accepted the privacy policy
+          </Link>
         </div>
       </div>
     </section>
